c30a7eb7d6b7e7a17a291461f7b4292f4f96b259
unknown <lauracolqueronaldnicolas@gmail.com>
2024-03-02 23:02:19 -0400

    TAREA 02/03/2024 PROLOG

---
+++ universidad.pl
@@ -0,0 +1,49 @@
+mario.
+arturica.
+andres.
+carla.
+magenta.
+giovana.
+elena.
+
+programacion.
+redes.
+inteligenciaartificial.
+
+esHombre(mario).
+esHombre(andres).
+
+esMujer(arturica).
+esMujer(carla).
+esMujer(magenta).
+esMujer(giovana).
+esMujer(elena).
+
+enseniaMateria(mario).
+enseniaMateria(arturica).
+enseniaMateria(andres).
+
+pasaClases(carla).
+pasaClases(magenta).
+pasaClases(giovana).
+pasaClases(elena).
+
+enseniaMateria(programacion, mario).
+enseniaMateria(redes, mario).
+
+enseniaMateria(programacion, arturica).
+enseniaMateria(inteligenciaartificial, arturica).
+
+enseniaMateria(redes, andres).
+
+pasaClases(inteligenciaartificial, carla).
+pasaClases(redes, carla).
+
+pasaClases(programacion, magenta).
+pasaClases(redes, magenta).
+pasaClases(inteligenciaartificial, magenta).
+
+pasaClases(redes, giovana).
+pasaClases(inteligenciaartificial, giovana).
+
+pasaClases(inteligenciaartificial, elena).
