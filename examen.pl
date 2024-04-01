ced847a959bae74423fc14717fbe10c0bde1d52f
unknown <lauracolqueronaldnicolas@gmail.com>
2024-04-01 00:05:01 -0400

    REPASO EXAMEN

---
+++ examen.pl
@@ -0,0 +1,53 @@
+entretenimiento.
+electrodomestico.
+batidora.
+lavadora.
+parlantes.
+
+producto.
+
+esProducto("LG-510").
+esProducto("Samsung B125").
+esProducto("LG Batimix").
+esProducto("Samsung L-200").
+esProducto("Bose").
+esProducto("LG").
+
+esCategoria(entretenimiento).
+esCategoria(electrodomestico).
+esCategoria(batidora).
+esCategoria(lavadora).
+esCategoria(parlantes).
+esCategoria(producto).
+
+estaEnCategoria("LG-510", batidora).
+estaEnCategoria("Samsung B125", batidora).
+estaEnCategoria("LG Batimix", lavadora).
+estaEnCategoria("Samsung L-200", lavadora).
+estaEnCategoria("Bose", parlantes).
+estaEnCategoria("LG", parlantes).
+
+estaEnCategoria(batidora, electrodomestico).
+estaEnCategoria(lavadora, electrodomestico).
+estaEnCategoria(parlantes, entretenimiento).
+
+estaEnCategoria(electrodomestico, producto).
+estaEnCategoria(lavadora, producto).
+
+esProductoDentrodeCategoria(Producto, Categoria):-esProducto(Producto),esCategoria(Categoria),estaEnCategoria(Producto, Categoria).
+esProductoDentrodeCategoria(Producto, Categoria):-estaEnCategoria(Categoria2, Categoria),esProductoDentrodeCategoria(Producto, Categoria2).
+
+tieneCaracteristicas("LG-510", blanco, barato, bueno).
+tieneCaracteristicas("Samsung B125", negro, caro, malo).
+tieneCaracteristicas("LG Batimix", rojo, barato, malo).
+tieneCaracteristicas("Samsung L-200", negro, caro, bueno).
+tieneCaracteristicas("Bose", blanco, caro, bueno ).
+tieneCaracteristicas("LG", rojo, barato, malo).
+
+recomendarAMaria1(Producto):-esProducto(Producto),tieneCaracteristicaS(Producto, verde, _ , _ ),esProductoDentroDeCategoria(Producto, producto).
+
+recomendarAMaria2(Producto):-esProducto(Producto),tieneCaracteristicaS(Producto, blanco, barato , _ ),esProductoDentroDeCategoria(Producto, entretenimiento).
+
+recomendarAMaria3(Producto):-esProducto(Producto),tieneCaracteristicaS(Producto, rojo, _ , malo ),esProductoDentroDeCategoria(Producto, electrodomestico).
+
+recomendarAMaria4(Producto):-esProducto(Producto),tieneCaracteristicaS(Producto, _ , _ , caro ),esProductoDentroDeCategoria(Producto, producto).
